67604e8f6e6065cb7e640bbe819362f09e7dd99a
Sachidananda Panigrahi <snpanigrahi88@gmail.com>
2015-04-02 21:55:19 +0530

    working on user controller

--- js/routes/index.js
+++ js/routes/index.js
@@ -1,8 +1,11 @@
+var userController = require('../controller/userController').UserController;
 
 module.exports.login = function (req, res) {
 	res.sendfile('login.html');
 }
 
 module.exports.user = function (req, res) {
-	
+	userController.getAllUsers().done(function (users) {
+        console.log(users);
+    });
 }
